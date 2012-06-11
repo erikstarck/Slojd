f305eeb10cbc39953304e82ac0837c154cdf2724
Erik Starck <erik.starck+github@gmail.com>
2012-06-11 17:21:41 +0200

    Changes to profile page

--- sites/all/themes/slojd/templates/user-profile.tpl.php
+++ sites/all/themes/slojd/templates/user-profile.tpl.php
@@ -1,52 +1,63 @@
 <div class="profile">
-  	<?php //debug($user_profile['flags' ]); ?>
-	<?php global $user;?>
-	<?php $user_profile['flags']['#title'] = null; ?>
-	<?php $user_profile['flags']['intesnall_anvandare']['#title'] = null; ?>
-	<?php $user_profile['flags']['kompis']['#title'] = null; ?>
+    <?php //debug($user_profile['flags' ]); ?>
+    <?php global $user; ?>
+    <?php $user_profile['flags']['#title'] = null; ?>
+    <?php $user_profile['flags']['intesnall_anvandare']['#title'] = null; ?>
+    <?php $user_profile['flags']['kompis']['#title'] = null; ?>
 
-	
+    <?php hide($user_profile['field_friendrelation']); ?>
 
-<div class="grid-16">
-	<h3><?php echo t("Profile");?></h3>
-		<?php
-//only render if not currently logged in user
-	if ($user_profile['field_profilename']['#object']->uid  != $user->uid) {
-		?>
-			<div class="grid-16">
-		<?php
-		print render($user_profile['flags' ]);
-		?>
-		</div>
-<?php }
-else hide($user_profile['flags' ]);?>
+    <div class="grid-24">
+        <h3><?php echo t("Profile"); ?></h3>
+    </div>
 
-	<?php //print flag_create_link('intesnall_anvandare', '1'); ?>
-	<?php print render($user_profile['user_picture']); ?>
+    <div class="grid-24">
+        <div class="grid-6">
+            <?php print render($user_profile['user_picture']); ?>
 
-	<?php print render($user_profile['field_profilename']); ?>
-	<?php print render($user_profile['field_surname']); ?>
-	<?php print render($user_profile['field_gender']); ?>
+            <?php print render($user_profile['field_profilename']); ?>
+            <?php print render($user_profile['field_surname']); ?>
+            <?php print render($user_profile['field_gender']); ?>
+        </div>
+        <div class="grid-16">
+            <div class="grid-14 silverline">
+                <?php //The numberofmessages-session variabel is set in the messages block, at the top in the header.?>
+                <h3><?php echo t("My messages");?></h3>
+                  <?php echo t("Number of messages: ".$_SESSION["numberofmessages"] ); ?>
 
-	<?php print render($user_profile['field_teknikintresse']); ?>
-	<?php print render($user_profile['field_materialintresse']); ?>
-	<?php print render($user_profile['field_verktygsintresse']); ?>
+            </div>
+            <div class="grid-14 silverline">
+                <h3><?php echo t("Friends"); ?></h3>
+                <?php
+                //only render if not currently logged in user
+                if ($user_profile['field_profilename']['#object']->uid != $user->uid) {
+                    ?>
+                    <div class="grid-16">
+                        <?php
+                        print render($user_profile['flags']);
+                        ?>
+                    </div>
+                <?php
+                }
+                else
+                    hide($user_profile['flags']);
+                ?>                
+                <?php
+                $user_profile['field_friendrelation']['#title'] = null;
+                print render($user_profile['field_friendrelation']);
+                ?>
+            </div>
 
+            <div class="grid-14 silverline">
+                <h3><?php echo t("My interests"); ?></h3>
+                <?php print render($user_profile['field_teknikintresse']); ?>
+                <?php print render($user_profile['field_materialintresse']); ?>
+                <?php print render($user_profile['field_verktygsintresse']); ?>
+            </div>
 
-	<?php  hide($user_profile['field_friendrelation' ]); ?>
-	
-	<?php //print render($user_profile); ?>
-  	<?php //debug($user_profile); ?>
-</div>
+        </div>
+    </div>
 
-
-<div class="grid-7">
-	<h3><?php echo t("Friends");?></h3>
-	<?php
-	$user_profile['field_friendrelation' ]['#title'] = null;
-	print render($user_profile['field_friendrelation' ]); ?>
-</div>
-
-<div class="grid-24">&nbsp;</div>
+    <div class="grid-24">&nbsp;</div>
 
 </div>
